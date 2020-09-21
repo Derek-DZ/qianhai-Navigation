d25feca3f4468c4c0e219f01f88a62b124221034
Derek <18201961626@163.com>
2020-09-21 10:17:57 +0800

    2020-09-21 版本号 2020-01

---
+++ src/main.js
@@ -0,0 +1,148 @@
+const $siteList = $('.siteList')
+const $siteLi = $siteList.find('li.last')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+const hashMap = xObject || [
+    {name : 'A', url: 'https://www.acfun.com'},
+    {name : 'B', url: 'https://bilibili.com'}
+]
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '')
+}
+const render = ()=>{
+    $siteList.find('li:not(.last)').remove()
+    hashMap.forEach((node, index)=>{
+            const $li = $(`
+            <li class="list">
+                <div class="labelButton">
+                    <div class="name">${node.name}</div>
+                    <div class="url">${simplifyUrl(node.url)}</div>
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-more"></use>
+                    </svg>
+                </div>
+            </li>
+            `).insertBefore($siteLi[0])
+            $li.on('click', ()=>{
+                window.open(node.url)
+            })
+
+            //监听Mobile长按事件和点击事件
+            let longClick = 0;
+            let timeOutEvent = 0;
+            $li.on({
+                touchstart: (e)=>{
+                    longClick=0;//设置初始为0
+                    timeOutEvent = setTimeout(()=>{
+                        $('.popWrapper').css('display', 'block')
+                        longClick=1;//假如长按，则设置为1
+                    },500);
+                    $('div.popTitle')[0].innerText='修改快捷标签'
+                    $('button.labelFinish')[0].innerText='修改'
+                    $('.popNameInput')[0].value = node.name
+                    $('.popUrlInput')[0].value = node.url
+                    let index = hashMap.indexOf(node);
+                    $('.labelFinish').on('click', () => {
+                        let newUrl = $('#popUrlInput').val()
+                        let newName = $('#popNameInput').val()
+                        if(!newName){
+                            hashMap[index].name=simplifyUrl(newUrl)[0].toUpperCase(),
+                            hashMap[index].url=newUrl
+                            console.log(name)
+                        }else{
+                            hashMap[index].name=newName,
+                            hashMap[index].url=newUrl
+                        }
+                    })
+                    $('.labelRemove').on('click', () => {
+                        hashMap.splice(index, 1)
+                        render()
+                    })
+                },
+                touchmove: (e)=>{
+                    clearTimeout(timeOutEvent);
+                    timeOutEvent = 0;
+                    e.preventDefault()
+                },
+                touchend: (e)=>{
+                    clearTimeout(timeOutEvent);
+                    if(timeOutEvent !== 0 && longClick === 0){
+                        window.open(node.url)
+                    }
+                    return false;
+                }
+            });
+            $li.on('click', '.icon', (e) => {
+                e.stopPropagation() // 阻止冒泡
+                $('.popWrapper').css('display', 'block')
+                $('div.popTitle')[0].innerText='修改快捷标签'
+                $('button.labelFinish')[0].innerText='修改'
+                $('.popNameInput')[0].value = node.name
+                $('.popUrlInput')[0].value = node.url
+                let index = hashMap.indexOf(node)
+                $('.labelFinish').on('click', () => {
+                    let newUrl = $('#popUrlInput').val()
+                    let newName = $('#popNameInput').val()
+                    if(!newName){
+                        hashMap[index].name=simplifyUrl(newUrl)[0].toUpperCase(),
+                        hashMap[index].url=newUrl
+
+                    }else{
+                        hashMap[index].name=newName,
+                        hashMap[index].url=newUrl
+                    }
+                })
+                $('.labelRemove').on('click', () => {
+                    hashMap.splice(index, 1)
+                    render()
+                })
+            })
+        }
+    )
+}
+
+render()
+
+// 弹出框事件设置
+$('.addButton').on('click', () => {
+    $('.popWrapper').css('display', 'block')
+    $('div.popTitle')[0].innerText='添加快捷标签'
+    $('button.labelFinish')[0].innerText='添加'
+})
+$('.labelFinish').on('click', () => {
+    if($('button.labelFinish')[0].innerText==='添加'){
+        let url = $('#popUrlInput').val()
+        let name = $('#popNameInput').val()
+        if(url){
+            if (url.indexOf('http') !== 0) {
+                url = 'https://' + url
+            }
+            if(!name){
+                hashMap.push({
+                    name: simplifyUrl(url)[0].toUpperCase(),
+                    url: url
+                })
+                console.log(name)
+            }else{
+                hashMap.push({
+                    name: name,
+                    url: url
+                })
+            }
+            render()
+        }
+    }
+})
+
+$('.labelCancel').on('click', () => {
+    $('.popWrapper').css('display', 'none')
+})
+
+window.onbeforeunload = () => {
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x', string)
+}
+
